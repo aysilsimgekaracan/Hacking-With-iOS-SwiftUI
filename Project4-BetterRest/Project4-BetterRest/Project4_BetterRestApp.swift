005ca6f119aa02cb44f55892b5c1a07f645259a7
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-07-08 17:05:19 +0300

    Project 4: BetterRest - Before Wrap Up

---
+++ Project4-BetterRest/Project4-BetterRest/Project4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project4_BetterRestApp.swift
+//  Project4-BetterRest
+//
+//  Created by Ayşıl Simge Karacan on 3.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
