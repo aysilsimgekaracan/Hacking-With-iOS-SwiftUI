a1b25f7b0f7b080aa04e06a7a5d058e82685edda
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-07-22 21:35:23 +0300

    Before Wrap Up - Project 5: Word Scramble

---
+++ Project5-WordScramble/Project5-WordScramble/Project5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project5_WordScrambleApp.swift
+//  Project5-WordScramble
+//
+//  Created by Ayşıl Simge Karacan on 8.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
