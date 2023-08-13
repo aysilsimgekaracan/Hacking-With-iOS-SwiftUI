64de42b0baa55febf18f41146a0090baf388f8ca
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-08-13 15:45:45 +0300

    Project 7: iExpense

--- Project7-iExpense/Project7-iExpense/ContentView.swift
+++ Project7-iExpense/Project7-iExpense/ContentView.swift
@@ -7,28 +7,49 @@
 
 import SwiftUI
 
+struct ExpenseSection: View {
+    let header: String
+    let expenseType: String
+    let expenses: [ExpenseItem]
+    let onRemove: (IndexSet) -> Void
+    
+    var body: some View {
+        Section {
+            ForEach(expenses) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text(item.type)
+                    }
+                    Spacer()
+                    
+                    Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundColor(item.amount > 10 && item.amount < 100  ? .yellow : (item.amount <= 10 ? .black : .red))
+                }
+                
+            }
+            .onDelete(perform: onRemove)
+        } header : {
+            Text(header)
+        }
+    }
+}
+
 struct ContentView: View {
     @StateObject var expenses = Expenses()
     @State private var showingAddExpense = false
-   
+    
     var body: some View {
         NavigationView {
             VStack {
                 List {
-                    ForEach(expenses.items) { item in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(item.name)
-                                    .font(.headline)
-                                Text(item.type)
-                            }
-                            Spacer()
-                            
-                            Text(item.amount, format: .currency(code: "USD"))
+                    ForEach(Array(getAllExpenseTypes()), id: \.self) { type in
+                        ExpenseSection(header: "\(type) Expenses", expenseType: "\(type)", expenses: expensesForType(type)) { indexSet in
+                            removeItems(at: indexSet, for: type)
                         }
-                        
                     }
-                    .onDelete(perform: removeItems)
+
                 }
             }
             .navigationTitle("iExpense")
@@ -46,10 +67,22 @@ struct ContentView: View {
         
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func getAllExpenseTypes() -> Set<String>  {
+        Set(expenses.items.map {$0.type})
     }
-
+    
+    func expensesForType(_ type: String) -> [ExpenseItem] {
+        expenses.items.filter {
+                $0.type == type
+            }
+        }
+    
+    func removeItems(at offsets: IndexSet, for type: String) {
+        let chosenElement = expensesForType(type)[offsets.first!]
+        let uuid = chosenElement.id
+        let index = expenses.items.firstIndex(where: { $0.id == uuid })!
+        expenses.items.remove(at: index)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
